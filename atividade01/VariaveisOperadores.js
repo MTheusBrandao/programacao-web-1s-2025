54be87817b98ce74c79102a43e9842b016bcaa24
MTheusBrandao <matheusunai@gmail.com>
2025-03-18 09:35:02 -0300

    feat: atividade1

---
+++ atividade01/VariaveisOperadores.js
@@ -0,0 +1,14 @@
+let num_A = 10
+let num_B = 5
+
+let soma = num_A + num_B
+let multiplicacao = num_A * num_B
+let subtracao = num_A - num_B
+let divisao = num_A / num_B
+let resto = num_A % num_B
+
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${soma}`)
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${multiplicacao}`)
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${subtracao}`)
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${divisao}`)
+console.log(`Resultado do resto entre ${num_A} e ${num_B} é ${resto}`)
